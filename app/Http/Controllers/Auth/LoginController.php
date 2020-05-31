c44e52aefeb895913d4d2d314ba5bcb825f5fdbe
Rezky Maulana <rezkymaulanaa06@gmail.com>
2020-05-31 22:41:19 +0700

    Update LoginController and login.blade

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers\Auth;
 
+/* Use Illuminate */
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request; 
+use Illuminate\Validation\Rule;
+
+/* Use Auth */
+use Auth;
+
+/* Use User Model */
+use App\User;
+use App\user\CheckStatus;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
@@ -36,5 +47,161 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Login function
+     *
+     * @return void
+     */
+    public function login(Request $request)
+    {
+        /** 
+         * Request Credentials
+         */
+        $username   = $request->username;
+        $email      = $request->username;
+        $password   = $request->password;
+        $isEmail    = filter_var($email, FILTER_VALIDATE_EMAIL);
+
+        /**
+         * Get Account From Database
+         */
+        $getByEmail     = User::whereEmail($email)->first();
+        $getByUsername  = User::whereUsername($username)->first();
+
+        /**
+         * Get Account Status
+         */
+        $userStatus = $this->checkStatusAccount($username);
+
+        /**
+         * Filter Credentials
+         * 
+         * withError
+         * this function is use key name="username" in login.blade.php
+         */
+        if($getByEmail == false AND $isEmail == true)
+        {
+            /**
+             * If Email not found
+             */
+            return redirect()->back()->withErrors(['username' => 'Email tidak di temukan'])->withInput();
+            
+        } else if($getByUsername == false AND $isEmail == false)
+        {
+            /**
+             * If Username notfound
+             */
+            return redirect()->back()->withErrors(['username' => 'Username tidak di temukan'])->withInput();
+
+        } else if($userStatus['status'] != true)
+        {
+            /**
+             * If status account is not actived
+             */
+            return redirect()->back()->withErrors(['error' => $userStatus['msg']])->withInput();
+
+        } else 
+        {
+            $loginUsing = ($getByEmail) ? $email : $username;
+            $loginKey   = ($getByEmail) ? 'email' : 'username';
+        }
+
+        /**
+         * Login Process
+         */
+        if (Auth::attempt([$loginKey => $loginUsing, 'password' => $password])) 
+        {
+            
+            /**
+             * If Login successfuly, redirect to dashboard
+             */
+            $user = auth()->user();
+            $home = route($user->role . '.home');
+            return redirect()->intended($home);
+        }
+
+        /**
+         * If failed to login turn back to login form
+         * and give error password
+         */
+        return redirect()->back()->withInput($request->only('username'))->withErrors(
+        [
+            'password' => 'Kata sandi salah',
+        ]);
+
+        /**
+         * If login is successfuly then redirect to home
+         */
+        //return redirect()->route('home');
+
+    }
+
+    /**
+     * Check Account Status Function
+     *
+     * @return void
+     */
+    private function checkStatusAccount($username)
+    {
+        /**
+         * Get user account information
+         */
+        $getAccount = User::where('username', $username)
+        ->orWhere('email', $username)
+        ->first();
+
+        /**
+         * Check if user avaible
+         */
+        if(!empty($getAccount))
+        {
+            $userStatus = $getAccount->status;
+
+        } else 
+        {
+            $userStatus = 'notfound';
+        }
+
+        /**
+         * Filter status
+         */
+        switch ($userStatus) {
+            case 'nonactive':
+                return [
+                    'status'    => false,
+                    'msg'       => 'Mohon maaf akun kamu belum aktif, hubungi layanan bantuan'
+                ];
+                break;
+
+            case 'suspended':
+                return [
+                    'status'    => false,
+                    'msg'       => 'Mohon maaf akun kamu di tangguhkan, hubungi layanan bantuan'
+                ];
+                break;
+
+            case 'deleted':
+                return [
+                    'status'    => false,
+                    'msg'       => 'Mohon maaf akun kamu sudah terhapus, hubungi layanan bantuan'
+                ];
+                break;
+
+            case 'notfound':
+                return [
+                    'status'    => false,
+                    'msg'       => 'Akun tersebut belum terdaftar'
+                ];
+                break;
+            
+            default:
+            return [
+                'status'    => true,
+                'msg'       => 'ACCOUNT_STATUS_CONFIRMED'
+            ];
+                break;
+        }
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,111 @@
-@extends('layouts.app')
+@extends('layouts.app_auth')
 
+{{-- Title head --}}
+@section('title_head', 'Masuk')
+
+{{-- Content --}}
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="row">
+        <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+        <div class="login-brand">
+            {{-- <img src="assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle"> --}}
+        </div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+        <div class="card card-primary">
+            <div class="card-header"><h4> Masuk </h4></div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+            <div class="card-body">
+                {{-- Show Error --}}
+                
+                @if ($errors->any())
+                    <div class="alert alert-danger alert-dismissible show fade">
+                        <div class="alert-body">
+                        <button class="close" data-dismiss="alert">
+                            <span>×</span>
+                        </button>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                        </div>
+                    </div>
+                @endif
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
+                    {{-- CSRF --}}
+                    @csrf
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                    <div class="form-group">
+
+                        <label for="username"> Email / Username </label>
+                        <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" tabindex="1" required autofocus>
+                        
+                        {{-- if username/email error --}}
+                        @error('username')    
+                            <div class="invalid-feedback">
+                                
+                            </div>
+                        @enderror
+
+                    </div>
+
+                    <div class="form-group">
+
+                        <div class="d-block">
+                            <label for="password" class="control-label"> Kata sandi </label>
+                            <div class="float-right">
+                            <a href="javascript:void(0);" class="text-small">
+                                Lupa kata sandi? 
+                            </a>
                             </div>
                         </div>
+                        <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        {{-- if password error --}}
+                        @error('password')
+                            <div class="invalid-feedback">
+                                
                             </div>
+                        @enderror
+                        
+                    </div>
+                    
+                    <div class="form-group">
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
+                            <label class="custom-control-label" for="remember-me"> Ingat saya </label>
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                        Masuk
+                    </button>
+                    </div>
+                </form>
+            <div class="text-center mt-4 mb-3">
+                <div class="text-job text-muted"> Masuk dengan sosial media </div>
+            </div>
+            <div class="row sm-gutters">
+                <div class="col-6">
+                <a class="btn btn-block btn-social btn-facebook">
+                    <span class="fab fa-facebook"></span> Facebook
+                </a>
+                </div>
+                <div class="col-6">
+                <a class="btn btn-block btn-social btn-google">
+                    <span class="fab fa-google"></span> Google
+                </a>                                
                 </div>
             </div>
+
+            </div>
+        </div>
+        <div class="mt-5 text-muted text-center">
+            Tidak punya akun? <a href="{{ route('register') }}"> Daftar Gratiss! </a>
+        </div>
+        <div class="simple-footer">
+            Copyright &copy; GazzPay {{ date('Y') }} with Stisla 2018 template
+        </div>
         </div>
     </div>
-</div>
 @endsection
