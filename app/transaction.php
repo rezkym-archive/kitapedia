180ff2d05798bc6d219774e9a6fbcba10e853f68
Rezky Maulana <rezkymaulanaa06@gmail.com>
2020-06-04 10:33:31 +0700

    modified transaction model

--- app/transaction.php
+++ app/transaction.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class transaction extends Model
 {
-    //
+    protected $table = "transaction";
+
+    public function transactionPPOB()
+    {
+        return $this->belongsTo('App\admin\trx\transactionPPOB', 'ppob_id');
+    }
+
+    public function transactionSosmed()
+    {
+        return $this->belongsTo('App\admin\trx\transactionSosmed', 'sosmed_id');
+    }
 }
